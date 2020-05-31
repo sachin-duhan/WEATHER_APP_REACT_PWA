5f07608fa70c52806a7c52b5c984cc77cd23fa92
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-01 04:42:55 +0530

    Updates

---
+++ precache-manifest.48a35c6325c3b6ec77984b4a15a22986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac913a6c7616fd5c43cc4379620bdfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acdf8509d377a30784d5",
+    "url": "/static/css/main.a7f9d543.chunk.css"
+  },
+  {
+    "revision": "639dcefffa8802f1a99e",
+    "url": "/static/js/2.73b3352e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73b3352e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdf8509d377a30784d5",
+    "url": "/static/js/main.6ecd81aa.chunk.js"
+  },
+  {
+    "revision": "f355b9485550b217d62a",
+    "url": "/static/js/runtime-main.61938b12.js"
+  }
+]);
